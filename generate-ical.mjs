bfdcd0a76dee8ef2b81f8116b8b4a5bf2665ebc9
Fatma Degirmenci <fatosdegirmenci644@gmilcom>
2025-10-15 15:10:30 +0100

    Implement function to create .ics file

--- generate-ical.mjs
+++ generate-ical.mjs
@@ -1,7 +1,41 @@
-// This is a placeholder file which shows how you can access functions and data defined in other files. You can delete the contents of the file once you have understood how it works.
-// It can be run with `node`.
-
-import { getGreeting } from "./common.mjs";
+import fs from "node:fs";
+import { getCommemorativeDatesForYear } from "./common.mjs";
 import daysData from "./days.json" with { type: "json" };
 
-console.log(`{getGreeting()} - there are ${daysData.length} known days`);
+let icsTitle=`BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//CommemorativeDays//EN
+`;
+
+for(let year=2020; year<=2030; year++){
+    const comDay = getCommemorativeDatesForYear(year, daysData);
+
+    for(const [key,value] of Object.entries(comDay)){
+        const part = key.split("-");
+        const fullDatePart = part.join("");
+
+        const uidName = value.split(" ").join("-");
+        const uid = `${fullDatePart}-${uidName}@commemorative`;
+
+        icsTitle += `BEGIN:VEVENT
+UID:${uid}
+DTSTAMP:${fullDatePart}T000000Z
+DTSTART;VALUE=DATE:${fullDatePart}
+DTEND;VALUE=DATE:${getNextDay(fullDatePart)}
+SUMMARY:${value}
+END:VEVENT
+
+
+`;
+    }
+}
+function getNextDay(yyyymmdd) {
+    const year = Number(yyyymmdd.slice(0, 4));
+    const month = Number(yyyymmdd.slice(4, 6)) - 1;
+    const day = Number(yyyymmdd.slice(6, 8));
+    const date = new Date(year, month, day);
+    date.setDate(date.getDate() + 1);
+    return date.toISOString().slice(0, 10).split("-").join("");
+  }
+icsTitle +=`END:VCALENDAR`;
+fs.writeFileSync("days.ics", icsTitle, "utf8");
